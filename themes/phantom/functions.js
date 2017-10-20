797b6724339a3a1c70865b4ea6b25e1e6108cbf5
Yulian Purnama <ian.doang@gmail.com>
2017-10-20 15:07:39 +0700

    Init

---
+++ themes/phantom/functions.js
@@ -0,0 +1,9 @@
+import {registerWidgetArea} from '../../../src/includes/widgetUtils'
+
+const functions = function(){		
+	registerWidgetArea("Sidebar");
+	registerWidgetArea("Single");
+	registerWidgetArea("Footer");
+}
+
+export default functions
